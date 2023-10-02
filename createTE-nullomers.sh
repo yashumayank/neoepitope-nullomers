a6ac924b7de0efe0906b03837c3c7314717e476d
Mayank Mahajan <yashumayank@hotmail.com>
2023-10-02 15:44:49 -0400

    Update createTE-nullomers.sh

--- createTE-nullomers.sh
+++ createTE-nullomers.sh
@@ -25,12 +25,8 @@ awk '(FNR==NR){map[">"$5":"$6"-"$7]=$1"\t"$11;next}{if($0~/^>/){print $1"\t"map[
 awk '{if(NR==FNR){a[$1]=1;next}{if($0~/^>.*/){count=0;seqlen=length(seq)-15;for(x=1;x<=seqlen;x++){y=substr(seq,x,16);if(a[y]!=1){print y;count++}};seq=""; print count > "test"}else{seq=seq $1}}}END{count = 0;seqlen=length(seq)-15;for(x=1;x<=seqlen;x++){y=substr(seq,x,16);if(a[y]!=1){print y;count++}};print count > "test"}' ../Homo_sapiens_cds_16mers.tab TE_seqs_named.fasta > TE_nullomers.all.tsv
 #Calculate frequency of each nullomer in the genome
 awk -F "\t" '(NR==FNR){a[$1]=1;next}{if($1~/^>.*/){if(seqid!=""){for(i=1;i<=(length(seq)-15);i++){xtr=substr(seq,i,16);if(xtr in a){a[xtr]++}}};seqid=$1;seq=""}else{seq=seq""$1}}END{for(j=1;j<=(length(seq)-15);j++){xtr=substr(seq,j,16);if(xtr in a){a[xtr]++}};for(k in a){print k"\t"a[k]-1}}' TE_nullomers.all.tsv /data/hemberg/shared_resources/genomes/human/GRCh38.p13.107.genome.fa > TE_nullomers_freq.tsv
-#---Keep nullomers with genome-frequency > 300 and overlap <= 1 bp
-awk '{if(NR==FNR){a[$1]=$2;next}{if($0~/^>.*/){count=0;seqlen=length(seq)-15;for(x=1;x<=seqlen;x++){y=substr(seq,x,16);if(y in a){if(a[y]<300){print y;count++}}};seq=""; print count > "test"}else{seq=seq $1}}}END{count = 0;seqlen=length(seq)-15;for(x=1;x<=seqlen;x++){y=substr(seq,x,16);if(a[y]!=1){print y;count++}};print count > "test"}' TE_nullomers_freq.tsv TE_seqs_named.fasta > TE-nullomersSparse10.tsv
-
-awk -F "\t" '{if(NR==FNR){f[$1]=$2}else{split($1,u,";");c=0;nlist="";olist="";for(x=1;x<length(u);x++){if(f[u[x]]<300){nlist=nlist";"u[x];c++};if(c>6){split(substr(nlist,2),v,";");olist=v[1]";"v[2]";"v[3]";"v[length(v)-2]";"v[length(v)-1]";"v[length(v)]";"}else{olist=substr(nlist,2)}};print olist"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6}}' TE_nullomers_freq.tsv TE_nullomers.all.tsv > TE-nullomersSparse10.tsv
-
-awk '{split($1,u,";");for(i=1;i<length(u);i++)print toupper(u[i])}' ChimerDB-nullomersEdge6.tsv > ChimerDB_nullomers.tsv
+#---Keep nullomers with genome-frequency > 300 and overlap <= 6 bp; (leave 10b after every nullomer)
+awk '{if(NR==FNR){a[$1]=$2;next}{if($0~/^>.*/){count=0;seqlen=length(seq)-15;for(x=1;x<=seqlen;x++){y=substr(seq,x,16);if(y in a){if(a[y]<300){print y;count++;x+=10}}};seq=""; print count > "test"}else{seq=seq $1}}}END{count = 0;seqlen=length(seq)-15;for(x=1;x<=seqlen;x++){y=substr(seq,x,16);if(a[y]!=1){print y;count++}};print count > "test"}' TE_nullomers_freq.tsv TE_seqs_named.fasta| sort| uniq > TE-nullomers.tsv
 
 #revComp cds_nullomers to search on the 2nd read from RNAseq data
 awk 'BEGIN{c["A"]="T";c["T"]="A";c["G"]="C";c["C"]="G";}{y="";for(j=16;j>=1;j--){y=y c[substr($1,j,1)]};print y;}' TE_nullomers.tsv > TE_nullomersRevComp.tsv
